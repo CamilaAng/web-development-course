480dd3c2e22588bbaf82ae5017f8912d94adfef5
CamilaAng <115196280+CamilaAng@users.noreply.github.com>
2022-12-08 05:10:21 +0000

    Completes exercise_01

--- public/exercise_01/scripts/main.js
+++ public/exercise_01/scripts/main.js
@@ -1,6 +1,24 @@
 var celsiusInput = document.getElementById("celsius-input");
 var celsiusButton = document.getElementById("celsius-button");
 var fahrenheitInput = document.getElementById("celsius-to-fahrenheit-result");
-var kelvinInput = document.getElementById("calsius-to-kelvin-result");
+var kelvinInput = document.getElementById("celsius-to-kelvin-result");
 
-var changeCelsius = () => {}
+var celsiusToFahrenheit = (celsius) => {
+    var fahrenheit;
+    fahrenheit = (celsius * 1.8) + 32;
+    return fahrenheit;
+};
+
+var celsiusToKelvin = (celsius) => {
+    var kelvin;
+    kelvin = celsius + 273.15;
+    return kelvin;
+};
+
+celsiusButton.onclick = () => {
+    var celsius = celsiusInput.value.trim();
+    var fahrenheit = celsiusToFahrenheit(celsius);
+    var kelvin = celsiusToKelvin(celsius);
+    fahrenheitInput.value = fahrenheit;
+    kelvinInput.value = kelvin;
+};
