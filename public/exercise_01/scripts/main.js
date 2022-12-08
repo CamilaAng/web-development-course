1cc3f09500597aeba6e9c2aa43e1bf6792639457
CamilaAng <115196280+CamilaAng@users.noreply.github.com>
2022-12-08 04:12:18 +0000

    Adds basic linking

---
+++ public/exercise_01/scripts/main.js
@@ -0,0 +1,5 @@
+var celsiusInput = document.getElementById("celsius-input");
+var celsiusButton = document.getElementById("celsius-button");
+var fahrenheitInput = document.getElementById("celsius-to-fahrenheit-result");
+var kelvinInput = document.getElementById("calsius-to-kelvin-result");
+
