95081ed099142e4512d7cd126f51a6ab1ee7bb79
Camila Anguiano <camilaang98@gmail.com>
2022-12-10 10:54:07 -0600

    Adds serveIndex to show all exercises

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Dependencies
 const express = require('express');
+const serveIndex = require('serve-index');
 
 // Settings
 const PORT = process.env.PORT || 3000;
@@ -7,6 +8,9 @@ const PORT = process.env.PORT || 3000;
 // Main objects
 const app = express();
 
-app.use(express.static('public'));
+app.use(
+    express.static('public'),
+    serveIndex('public', { icons: true })
+);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
