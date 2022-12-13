ea4d487b0b314efbb73fe33e3b65031705cc4e06
Camila Anguiano <camilaang98@gmail.com>
2022-12-12 20:25:46 -0600

    Completes Exercise1

--- public/exercise_01/scripts/main.js
+++ public/exercise_01/scripts/main.js
@@ -16,9 +16,59 @@ var celsiusToKelvin = (celsius) => {
 };
 
 celsiusButton.onclick = () => {
-    var celsius = celsiusInput.value.trim();
+    var celsius = Number(celsiusInput.value.trim());
     var fahrenheit = celsiusToFahrenheit(celsius);
     var kelvin = celsiusToKelvin(celsius);
     fahrenheitInput.value = fahrenheit;
     kelvinInput.value = kelvin;
 };
+
+var fahrenheitInputTwo = document.getElementById("fahrenheit-input");
+var fahrenheitButton = document.getElementById("fahrenheit-button");
+var celsiusFahrenheitResult = document.getElementById("fahrenheit-to-celsius-result"); 
+var kelvinFahrenheitResult = document.getElementById("fahrenheit-to-kelvin-result");
+
+var fahrenheitToCelsius = (fahrenheit) => {
+    var celsius;
+    celsius = (fahrenheit - 32) * 5 / 9;
+    return celsius;
+}
+
+var fahrenheitToKelvin = (fahrenheit) => {
+    var celsius = fahrenheitToCelsius(fahrenheit);
+    var kelvin = celsiusToKelvin(celsius);
+    return kelvin;
+}
+
+fahrenheitButton.onclick = () => {
+    var fahrenheit = fahrenheitInputTwo.value.trim();
+    var celsius = fahrenheitToCelsius(fahrenheit);
+    var kelvin = fahrenheitToKelvin(fahrenheit);
+    celsiusFahrenheitResult.value = celsius;
+    kelvinFahrenheitResult.value = kelvin;
+}
+
+var kelvinInputTwo = document.getElementById("kelvin-input"); 
+var kelvinButton = document.getElementById("kelvin-button"); 
+var celsiusKelvinResult = document.getElementById("kelvin-to-celsius-result"); 
+var fahrenheitKelvinResult = document.getElementById("kelvin-to-fahrenheit-result");
+
+var kelvinToCelsius = (kelvin) => {
+    var celsius; 
+    celsius = kelvin - 273.15;
+    return celsius; 
+}
+
+var kelvinToFahrenheit = (kelvin) => { 
+    var celsius = kelvinToCelsius(kelvin);
+    var fahrenheit = celsiusToFahrenheit(celsius)
+    return fahrenheit;
+}
+
+kelvinButton.onclick = () => { 
+    var kelvin = kelvinInputTwo.value.trim();
+    var celsius = kelvinToCelsius(kelvin);
+    var fahrenheit = kelvinToFahrenheit(kelvin);
+    celsiusKelvinResult.value = celsius;
+    fahrenheitKelvinResult.value = fahrenheit;
+}
